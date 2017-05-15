2a45786aaae2d7e5e7fa5c2474e0e08418cb9002
deltafunction <frederik@orellana.dk>
2017-05-15 20:54:08 +0200

    Webdav auth fix.

--- appinfo/remote.php
+++ appinfo/remote.php
@@ -22,7 +22,8 @@ OCP\JSON::checkLoggedIn();
 $userServerAccess = \OCA\FilesSharding\Lib::getUserServerAccess();
 // Block all access if account is locked on server
 if($ok && \OCP\App::isEnabled('files_sharding') &&
-		$userServerAccess!=\OCA\FilesSharding\Lib::$USER_ACCESS_ALL){
+		$userServerAccess!=\OCA\FilesSharding\Lib::$USER_ACCESS_ALL &&
+		$userServerAccess!=\OCA\FilesSharding\Lib::$USER_ACCESS_READ_ONLY){
 	$ok = false;
 }
 
