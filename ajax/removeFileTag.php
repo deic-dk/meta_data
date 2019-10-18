73004d95404002039ab53f37f7775a2035f9969b
Frederik Orellana <frederik@orellana.dk>
2019-10-18 13:36:42 +0200

    Return something...

--- ajax/removeFileTag.php
+++ ajax/removeFileTag.php
@@ -8,3 +8,5 @@ $fileid = isset( $_GET['fileid'] ) ? $_GET['fileid'] : '';
 $tagid =  isset( $_GET['tagid'] ) ? $_GET['tagid'] : '';
 
 $result = \OCA\meta_data\Tags::removeFileTag($tagid,$fileid);
+
+OCP\JSON::success($result);
